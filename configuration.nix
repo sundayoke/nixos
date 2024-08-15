024baea60e8d49c3c12ce065d006bdd087b16bc8
Sunday Oke <sunday.oke@gmail.com>
2024-08-15 12:38:58 +0200

    saDCCDS

--- configuration.nix
+++ configuration.nix
@@ -9,6 +9,8 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./nix-ld.nix
+      ./apps.nix
+      ./xfce.nix
     ];
 
   # Bootloader.
@@ -54,7 +56,7 @@
   services.xserver.enable = true;
 
   # Enable XFCE
-  services.displayManager.sddm.wayland.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.xfce.enable = true;
 
   ## Enable the KDE Plasma Desktop Environment.
@@ -176,72 +178,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-   	vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  	wget
-    pkgs.vscode
-	  pkgs.displaylink
-    pkgs.keepassxc
-    pkgs.spotify
-    pkgs.bottles
-    pkgs.xfce.catfish
-    pkgs.zoom-us
-    pkgs.calibre
-    pkgs.gimp-with-plugins
-    pkgs.wpsoffice
-    pkgs.libreoffice
-    pkgs.google-chrome
-    pkgs.vlc
-    pkgs.insync
-    pkgs.dialog
-    pkgs.newt
-    pkgs.lsof
-    pkgs.samba4Full
-    pkgs.libxml2
-    pkgs.bc
-    pkgs.cabextract
-    pkgs.cryptomator
-    pkgs.wineWowPackages.stagingFull
-    pkgs.openvpn
-    pkgs.kdePackages.partitionmanager
-    pkgs.microsoft-edge
-    pkgs.python312Full
-    pkgs.git
-    pkgs.kdePackages.kdialog
-    pkgs.kdePackages.kdenlive
-    pkgs.fuse3
-    pkgs.lsof
-    pkgs.gnupatch
-    #pkgs.maestral
-    #pkgs.maestral-gui
-    #pkgs.onedrive
-    #pkgs.onedrivegui
-    #pkgs.xfce.thunar
-    #pkgs.zenity
-    #pkgs.kdePackages.kdevelop
-    #pkgs.discord
-    #pkgs.logseq
-    #pkgs.jupyter-all
-    #pkgs.python312Packages.python
-    #pkgs.python312Packages.pip
-    #pkgs.python312Packages.virtualenv
-    ## beginning of LXQT an i3 installation ##
-    #pkgs.tk
-    #pkgs.nm-tray
-    #pkgs.xfce.thunar
-    #pkgs.dolphin
-    #pkgs.rofi
-    #pkgs.terminator
-    #pkgs.konsole
-    #pkgs.lxde.lxrandr
-    #pkgs.i3blocks
-    #pkgs.konsole
-    #pkgs.feh
-    #pkgs.libsForQt5.polkit-kde-agent
-    #pkgs.polkit_gnome
-    #pkgs.polkit
-    #pkgs.libsForQt5.polkit-qt
-  ];
+
 
   fonts.packages = with pkgs; [
   noto-fonts
---
+++ xfce.nix
@@ -0,0 +1,59 @@
+{ pkgs, ... }:
+{
+  environment = {
+    systemPackages = with pkgs; [
+      pkgs.blueman
+      pkgs.git
+      chromium
+      deja-dup
+      drawing
+      elementary-xfce-icon-theme
+      evince
+      #pkgs.firefox
+      foliate
+      font-manager
+      gimp-with-plugins
+      gnome.file-roller
+      gnome.gnome-disk-utility
+      inkscape-with-extensions
+      libqalculate
+      libreoffice
+      orca
+      pavucontrol
+      qalculate-gtk
+      thunderbird
+      wmctrl
+      xclip
+      xcolor
+      xcolor
+      xdo
+      xdotool
+      xfce.catfish
+      xfce.gigolo
+      xfce.orage
+      xfce.xfburn
+      xfce.xfce4-appfinder
+      xfce.xfce4-clipman-plugin
+      xfce.xfce4-cpugraph-plugin
+      xfce.xfce4-dict
+      xfce.xfce4-fsguard-plugin
+      xfce.xfce4-genmon-plugin
+      xfce.xfce4-netload-plugin
+      xfce.xfce4-panel
+      xfce.xfce4-pulseaudio-plugin
+      xfce.xfce4-systemload-plugin
+      xfce.xfce4-weather-plugin
+      xfce.xfce4-whiskermenu-plugin
+      xfce.xfce4-xkb-plugin
+      xfce.xfdashboard
+      xorg.xev
+      xsel
+      xtitle
+      xwinmosaic
+      zuki-themes
+      pkgs.vscode
+    ];
+  };
+
+  sound.enable = true;
+}
